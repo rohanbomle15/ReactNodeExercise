b7471a313f0f997688ca3e59313f7027c7d6c2e6
Rohan Bomle <rohanbomle@gmail.com>
2023-08-18 12:15:57 +0100

    Integrate create task api

--- client/src/components/Creattask.jsx
+++ client/src/components/Creattask.jsx
@@ -1,17 +1,52 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Box, TextField, Button } from '@mui/material'
+import axios from "axios";
 
 const Creattask = () => {
+  const [data, setData] = useState({
+    task: ""
+  });
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setData({
+      ...data,
+      [e.target.name]: value
+    });
+  };
+
+  const onSubmitTask = (e) => {
+      const taskData = {
+        task: data.task
+      };
+
+      axios.post("/task", taskData)
+      .then((response) => {
+        console.log(response)
+        alert("New Task has been created..")
+      })
+      .catch((error) => {
+        if(error.response){
+          console.log(error.response)  
+        }else if(error.request) {
+          console.log(error.request)
+        }else {
+          console.log(error)
+        }
+      })
+  }
+
   return (
     <Box
       component="form"
+      onSubmit={onSubmitTask}
       sx={{
         '& > :not(style)': { m: 1, width: '25ch' },
       }}
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Create new Task" variant="outlined" />
+      <TextField id="outlined-basic" label="Create new Task" variant="outlined" value={data.task} onChange={handleChange}/>
       <Button variant="contained" disableElevation>Create Task</Button>
     </Box>
   )
